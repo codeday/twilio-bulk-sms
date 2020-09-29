19653357e1e87699b8fd1a00d6f3a888c74d87d2
Lola <lola@egherman.com>
2020-09-29 18:57:23 -0500

    add db stuff and more handlers

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
+import json
+
 from flask import Flask, request
 from flask_cors import CORS
 
+from db.models import session_creator, Group, Number
 from utils.request import validate_body
 from utils.response import response, error_response
 from utils.sms import send_bulk_sms
@@ -9,7 +12,8 @@ app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/message', methods=['POST'])
+
+@app.route('/api/message', methods=['POST'])
 def message_audience():
     body = request.get_json()
     status, missing_field = validate_body(body, ['message', 'phones'])
@@ -17,6 +21,68 @@ def message_audience():
         return error_response(f'{missing_field} is missing')
     send_bulk_sms(body['phones'], body['message'])
     return response(True, 'Success', None)
+
+
+@app.route('/api/getGroups', methods=['GET'])
+def get_groups():
+    session = session_creator()
+    return json.dumps(session.query(Group).all())
+
+
+@app.route('/api/createGroup', methods=['POST'])
+def create_group():
+    body = request.get_json()
+    assert 'group_name' in body
+    session = session_creator()
+    session.add(
+        Group(
+            group_name=body['group_name']
+        )
+    )
+    session.commit()
+    session.close()
+    return 'ok'
+
+
+@app.route('/api/addToGroup', methods=['POST'])
+def add_to_group():
+    body = request.get_json()
+    assert 'group_id' in body
+    assert 'number' in body
+    session = session_creator()
+    # Todo: verify number is number
+    group = session.query(Group).filter_by(id = body['group_id']).first()
+    if group is not None:
+        session.add(group)
+        group.numbers.append(Number(number=body['number']))
+        session.commit()
+        session.close()
+    else:
+        session.rollback()
+        session.close()
+        return 'no group found'
+
+
+@app.route('/api/removeFromGroup', methods=['POST'])
+def remove_from_group():
+    body = request.get_json()
+    assert 'group_id' in body
+    assert 'number' in body
+    session = session_creator()
+    group = session.query(Group).filter_by(id = body['group_id']).first()
+    if group is not None:
+        session.add(group)
+        session.query(Number).filter(
+            Number.number == body['number'], Number.group_id == body['group_id']
+        ).delete()
+        session.commit()
+        session.close()
+        return 'ok'
+    else:
+        session.rollback()
+        session.close()
+        return 'no group found'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80)
---
+++ src/db/models.py
@@ -0,0 +1,43 @@
+from os import getenv
+
+from sqlalchemy import *
+from sqlalchemy.engine.url import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session, relationship
+
+postgres_db = {
+    "drivername": "postgres",
+    "username": getenv("DB_USERNAME", "postgres"),
+    "password": getenv("DB_PASSWORD"),
+    "database": getenv("DB_DB", "twilio-bulk-sms"),
+    "host": getenv("DB_HOST", "10.0.3.34"),
+    "port": 5432,
+}
+postgres_url = URL(**postgres_db)
+engine = create_engine(postgres_url)
+metadata = MetaData()
+
+Base = declarative_base(bind=engine, metadata=metadata)
+
+
+class Group(Base):
+    __tablename__ = "group"
+    id = Column(Integer, primary_key=True)
+    group_name = Column(String, nullable=False, unique=True)
+    numbers = relationship("Number", back_populates="group", cascade="all, delete")
+
+
+class Number(Base):
+    __tablename__ = "number"
+    id = Column(Integer, primary_key=True)
+    group_id = Column(Integer, ForeignKey("group.id"))
+    number = Column(Integer, unique=True, nullable=False)
+    team = relationship("Team", back_populates="members", cascade="all, delete")
+
+
+def session_creator() -> Session:
+    session = sessionmaker(bind=engine)
+    return session()
+
+
+global_session: Session = session_creator()
